664edc18edcc774159c746355e2f2b4bc473bbf3
leo-dillon <jeannotegui@gmail.com>
2024-09-23 12:03:47 -0300

    Mejorando la interfaz del producto individual

--- vistas/producto.php
+++ vistas/producto.php
@@ -3,17 +3,29 @@
     require_once './clases/Funciones.php'; 
     $id = $_GET['id'];
     $producto = Productos::traerProductoID($id);
+    $num = Funciones::randomNum(3,12);
 
     // Funciones::mostrar($producto -> getDescription());
 ?>
 
-<section>
+<section class='producto'>
     <div>
         <picture>
             <img src="<?= $producto -> getThumbnail() ?>" alt="<?= $producto -> getTitle() ?>">
         </picture>
         <div class="info">
             <h1><?= $producto -> getTitle() ?></h1>
+            <h2>Categoria: <?= $producto -> getCategory() ?> </h2>
+            <p><strong> $ <?= $producto -> getPrice() ?></strong></p>
+            <p>Hasta <?= $num ?> cuotas <strong>SIN INTERÉS</strong></p>
+            <div class="cantidad">
+                <div>
+                    <button>-</button>
+                    <p>0</p>
+                    <button>+</button>
+                </div>
+                <button class="carrito">Agregar al carrito</button>
+            </div>
         </div>
     </div>
 </section>
